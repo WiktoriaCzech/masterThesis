4555c1f8e11df0affcc273e140d0c4836cf35c5c
Wiktoria Czech <czech.wiktoria1@gmail.com>
2025-03-24 16:51:59 +0100

    Feat: Added clarity integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Clarity from "@microsoft/clarity";
+
+const projectId = process.env.CLARITY_ID || "";
+
+Clarity.init(projectId);
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
