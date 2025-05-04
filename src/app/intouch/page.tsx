3820381f268aa9461214eb99105b10d7fadb7545
Wiktoria Czech <czech.wiktoria1@gmail.com>
2025-05-04 20:54:03 +0200

    Intouch questionare refactor

--- src/app/intouch/page.tsx
+++ src/app/intouch/page.tsx
@@ -1,21 +1,23 @@
 import Image from "next/image";
 import Footer from "@/components/footer/Footer";
-import styles from "../page.module.css";
+import styles from "./page.module.css";
 import zbiornik from "@/../public/intouch_zbiornik.png";
 
 export default function Intouch() {
   return (
-    <main className={`${styles.main} ${styles.overrideMain}`}>
-      <h3>
-        W którym miejscu kliknał byś na poniższym panelu operatorskim aby zmienić poziom wody w zbiorniku ?
+    <main className={styles.main}>
+      <h3 className={styles.intouchMessage}>
+        W którym miejscu kliknał byś na poniższym panelu operatorskim aby
+        zmienić poziom wody w zbiorniku ?
       </h3>
       <div className={`${styles.imageWrapper} ${styles.pointer}`}>
         <Image
           src={zbiornik}
           alt=""
           style={{
-            width: "auto",
-            height: "auto",
+            width: "100%",
+            maxWidth: "2000px",
+            height: "100%",
             objectFit: "contain",
           }}
           priority
